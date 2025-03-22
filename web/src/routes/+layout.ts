9ada8073cd0ec76f0de93f921c43597ba783386e
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2025-03-23 00:11:54 +0100

    init highlighter on shared page

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -1,8 +1,10 @@
 import type { LayoutLoad } from './$types'
+import { highlighter } from '$lib/shiki'
 import { initZ } from '$lib/zero'
 
 export const load: LayoutLoad = async () => {
   await initZ()
+  await highlighter.init()
 }
 
 export const ssr = false
